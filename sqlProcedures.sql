50c0b57b58e4e68ce95d7ff6e3875876830e646e
Victor Jimenez <vjimenez@americatv.com.pe>
2018-07-04 18:31:02 -0500

    ava

--- sqlProcedures.sql
+++ sqlProcedures.sql
@@ -45,3 +45,39 @@ SELECT TOP 1000 [codProyecto]
       ,[jefeProyecto]
   FROM [inspeccion].[dbo].[proyecto]
   END;
+
+/******tablas*****/
+
+USE [inspeccion]
+GO
+
+/****** Object:  Table [dbo].[proyecto]    Script Date: 07/04/2018 18:30:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[proyecto](
+	[codProyecto] [int] IDENTITY(1,1) NOT NULL,
+	[nombreProyecto] [varchar](50) NULL,
+	[fechaInicio] [date] NULL,
+	[fechaFin] [date] NULL,
+	[estado] [varchar](50) NULL,
+	[fechaCreacion] [date] NULL,
+	[usuarioCreacion] [varchar](50) NULL,
+	[jefeProyecto] [varchar](50) NULL,
+ CONSTRAINT [PK_proyecto] PRIMARY KEY CLUSTERED 
+(
+	[codProyecto] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
